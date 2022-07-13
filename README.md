8236ef6ec545a1bedc3d3f9f2c63a643d138017b
Cheng Ma <cheng.ma_2013@outlook.com>
2022-07-13 15:03:41 -0500

    add states with zero incidents to the dataset'

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 1. Data
 Datasets are downloaded from [GUN VIOLENCE ARCHIVE](https://www.gunviolencearchive.org/reports) on 7/7/2022.
 2. [SQL_gun_violence_data_exploration.sql](https://github.com/m724297540/SQL_gun_violence_data_exploration/blob/master/SQL_gun_violence_data_exploration.sql)
-performs data exploration using SQL. Skills used: Join, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Coverting Data Types.
+performs data exploration using SQL. Skills used: Join, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Coverting Data Types, Variables, While loops.
--- SQL_gun_violence_data_exploration.sql
+++ SQL_gun_violence_data_exploration.sql
@@ -1,7 +1,9 @@
 /*
 Gun Violence Data Exploration
-Skills used: Join, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Coverting Data Types
+Skills used: Join, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Coverting Data Types, Variables, While loops
 */
+USE sql_portfolio
+GO
 
 SELECT *
 FROM sql_portfolio..mass_shooting
@@ -93,9 +95,96 @@ FROM sql_portfolio..mass_shooting
 GROUP BY State, YEAR([Incident Date])
 ORDER BY CONVERT(DECIMAL(3,2), AVG([# Killed]))DESC;
 
+-- This dataset only includes states that has gun violence accidents
+-- The following query find states with zero gun violence accidents from 2019-02-17 to 2022-07-05
+-- Create a state look up table and then look up which state does not appear in the gun violence dataset
+-- Hawaii, North Dakota, Vermont have zero accident during the time range in the dataset
+
+DROP TABLE IF EXISTS sql_portfolio..StateLookup;
+CREATE TABLE sql_portfolio..StateLookup
+(
+   StateID       INT IDENTITY (1, 1),
+   StateName     VARCHAR (32),
+   StateAbbrev   CHAR (2),
+);
+
+INSERT INTO sql_portfolio..StateLookup
+VALUES ('Alabama', 'AL'),
+       ('Alaska', 'AK'),
+       ('Arizona', 'AZ'),
+       ('Arkansas', 'AR'),
+       ('California', 'CA'),
+       ('Colorado', 'CO'),
+       ('Connecticut', 'CT'),
+       ('Delaware', 'DE'),
+       ('District of Columbia', 'DC'),
+       ('Florida', 'FL'),
+       ('Georgia', 'GA'),
+       ('Hawaii', 'HI'),
+       ('Idaho', 'ID'),
+       ('Illinois', 'IL'),
+       ('Indiana', 'IN'),
+       ('Iowa', 'IA'),
+       ('Kansas', 'KS'),
+       ('Kentucky', 'KY'),
+       ('Louisiana', 'LA'),
+       ('Maine', 'ME'),
+       ('Maryland', 'MD'),
+       ('Massachusetts', 'MA'),
+       ('Michigan', 'MI'),
+       ('Minnesota', 'MN'),
+       ('Mississippi', 'MS'),
+       ('Missouri', 'MO'),
+       ('Montana', 'MT'),
+       ('Nebraska', 'NE'),
+       ('Nevada', 'NV'),
+       ('New Hampshire', 'NH'),
+       ('New Jersey', 'NJ'),
+       ('New Mexico', 'NM'),
+       ('New York', 'NY'),
+       ('North Carolina', 'NC'),
+       ('North Dakota', 'ND'),
+       ('Ohio', 'OH'),
+       ('Oklahoma', 'OK'),
+       ('Oregon', 'OR'),
+       ('Pennsylvania', 'PA'),
+       ('Rhode Island', 'RI'),
+       ('South Carolina', 'SC'),
+       ('South Dakota', 'SD'),
+       ('Tennessee', 'TN'),
+       ('Texas', 'TX'),
+       ('Utah', 'UT'),
+       ('Vermont', 'VT'),
+       ('Virginia', 'VA'),
+       ('Washington', 'WA'),
+       ('West Virginia', 'WV'),
+       ('Wisconsin', 'WI'),
+       ('Wyoming', 'WY')
+
+SELECT DISTINCT StateName
+FROM sql_portfolio..StateLookup
+WHERE StateName NOT IN (
+	SELECT State
+	FROM sql_portfolio..mass_shooting
+);
+
 -- Creating View to store data for later visualizations
 -- Columns: state, year, total_incidents, total_killed, total_injured, total_killed_child, total_injured_child, total_killed_teens, total_injured_teens
-CREATE VIEW gun_data_viz AS
+
+DROP TABLE IF EXISTS sql_portfolio..gun_data_viz_table;
+CREATE TABLE sql_portfolio..gun_data_viz_table(
+	state VARCHAR(225),
+	year INT,
+	total_incidents INT,
+	total_killed INT,
+	total_injured INT, 
+	total_ck INT,
+	total_ci INT,
+	total_tk INT,
+	total_ti INT
+)
+
+INSERT INTO sql_portfolio..gun_data_viz_table
 SELECT main.State AS state, YEAR(main.[Incident Date]) AS year, 
 	   COUNT(*) AS total_incidents,
 	   SUM(main.[# Killed]) AS total_killed, 
@@ -114,3 +203,203 @@ ON main.[Incident ID] = tk.[Incident ID]
 LEFT JOIN sql_portfolio..teens_injured ti
 ON main.[Incident ID] = ti.[Incident ID]
 GROUP BY main.State, YEAR(main.[Incident Date]);
+
+DROP VIEW IF EXISTS sql_portfolio..gun_data_viz;
+CREATE VIEW gun_data_viz AS
+SELECT * 
+FROM sql_portfolio..gun_data_viz_table;
+
+---------------------------------------
+-- Modify the view by adding new rows--
+---------------------------------------
+
+
+-- States have 0 incidents in 2019
+SELECT DISTINCT StateName,
+	ROW_NUMBER() OVER( ORDER BY StateName) AS rownum
+FROM sql_portfolio..StateLookup
+WHERE StateName NOT IN (
+	SELECT State
+	FROM sql_portfolio..mass_shooting
+	WHERE YEAR([Incident Date]) = 2019
+);
+
+-- States have 0 incidents in 2020
+SELECT DISTINCT StateName,
+	ROW_NUMBER() OVER( ORDER BY StateName) AS rownum
+FROM sql_portfolio..StateLookup
+WHERE StateName NOT IN (
+	SELECT State
+	FROM sql_portfolio..mass_shooting
+	WHERE YEAR([Incident Date]) = 2020
+);
+
+-- States have 0 incidents in 2021
+SELECT DISTINCT StateName,
+	ROW_NUMBER() OVER( ORDER BY StateName) AS rownum
+FROM sql_portfolio..StateLookup
+WHERE StateName NOT IN (
+	SELECT State
+	FROM sql_portfolio..mass_shooting
+	WHERE YEAR([Incident Date]) = 2021
+);
+
+-- States have 0 incidents in 2022
+SELECT DISTINCT StateName,
+	ROW_NUMBER() OVER( ORDER BY StateName) AS rownum
+FROM sql_portfolio..StateLookup
+WHERE StateName NOT IN (
+	SELECT State
+	FROM sql_portfolio..mass_shooting
+	WHERE YEAR([Incident Date]) = 2022
+);
+
+-- Create table variable to hold new written observations: states with 0 incidents
+DECLARE @table_addon TABLE (
+	state VARCHAR(225), 
+	year INT, 
+	total_incidents INT, 
+	total_killed INT, 
+	total_injured INT, 
+	total_ck INT, 
+	total_ci INT, 
+	total_tk INT,
+	total_ti INT
+)
+
+DECLARE @num2019 INT
+DECLARE @num2020 INT
+DECLARE @num2021 INT
+DECLARE @num2022 INT
+
+DECLARE @index INT
+DECLARE @name VARCHAR(225)
+
+SELECT @num2019 = COUNT(*)
+	FROM 
+		(SELECT DISTINCT StateName, 
+		ROW_NUMBER() OVER( ORDER BY StateName) AS rownum
+		FROM sql_portfolio..StateLookup
+		WHERE StateName NOT IN (
+			SELECT State
+			FROM sql_portfolio..mass_shooting
+			WHERE YEAR([Incident Date]) = 2019
+			)) t
+
+SELECT @num2020 = COUNT(*)
+	FROM 
+		(SELECT DISTINCT StateName, 
+		ROW_NUMBER() OVER( ORDER BY StateName) AS rownum
+		FROM sql_portfolio..StateLookup
+		WHERE StateName NOT IN (
+			SELECT State
+			FROM sql_portfolio..mass_shooting
+			WHERE YEAR([Incident Date]) = 2020
+			)) t
+
+SELECT @num2021 = COUNT(*)
+	FROM 
+		(SELECT DISTINCT StateName, 
+		ROW_NUMBER() OVER( ORDER BY StateName) AS rownum
+		FROM sql_portfolio..StateLookup
+		WHERE StateName NOT IN (
+			SELECT State
+			FROM sql_portfolio..mass_shooting
+			WHERE YEAR([Incident Date]) = 2021
+			)) t
+
+SELECT @num2022 = COUNT(*)
+	FROM 
+		(SELECT DISTINCT StateName, 
+		ROW_NUMBER() OVER( ORDER BY StateName) AS rownum
+		FROM sql_portfolio..StateLookup
+		WHERE StateName NOT IN (
+			SELECT State
+			FROM sql_portfolio..mass_shooting
+			WHERE YEAR([Incident Date]) = 2022
+			)) t
+			
+SET @index = 1
+WHILE @index <= @num2019
+BEGIN
+	SELECT @name = StateName FROM (
+		SELECT DISTINCT StateName, 
+		ROW_NUMBER() OVER( ORDER BY StateName) AS rownum
+		FROM sql_portfolio..StateLookup
+		WHERE StateName NOT IN (
+			SELECT State
+			FROM sql_portfolio..mass_shooting
+			WHERE YEAR([Incident Date]) = 2019
+			)) t
+	WHERE rownum = @index
+	PRINT @name
+	INSERT INTO @table_addon (state, year, total_incidents, total_killed, total_injured, total_ck, total_ci, total_tk, total_ti)
+	VALUES (@name, 2019, 0, 0, 0, 0, 0, 0, 0)
+	SELECT @index = @index + 1
+END
+
+SET @index = 1
+WHILE @index <= @num2020
+BEGIN
+	SELECT @name = StateName FROM (
+		SELECT DISTINCT StateName, 
+		ROW_NUMBER() OVER( ORDER BY StateName) AS rownum
+		FROM sql_portfolio..StateLookup
+		WHERE StateName NOT IN (
+			SELECT State
+			FROM sql_portfolio..mass_shooting
+			WHERE YEAR([Incident Date]) = 2020
+			)) t
+	WHERE rownum = @index
+
+	INSERT INTO @table_addon (state, year, total_incidents, total_killed, total_injured, total_ck, total_ci, total_tk, total_ti)
+	VALUES (@name, 2020, 0, 0, 0, 0, 0, 0, 0)
+	SELECT @index = @index + 1
+END
+
+SET @index = 1
+WHILE @index <= @num2021
+BEGIN
+	SELECT @name = StateName FROM (
+		SELECT DISTINCT StateName, 
+		ROW_NUMBER() OVER( ORDER BY StateName) AS rownum
+		FROM sql_portfolio..StateLookup
+		WHERE StateName NOT IN (
+			SELECT State
+			FROM sql_portfolio..mass_shooting
+			WHERE YEAR([Incident Date]) = 2021
+			)) t
+	WHERE rownum = @index
+
+	INSERT INTO @table_addon (state, year, total_incidents, total_killed, total_injured, total_ck, total_ci, total_tk, total_ti)
+	VALUES (@name, 2021, 0, 0, 0, 0, 0, 0, 0)
+	SELECT @index = @index + 1
+END
+
+SET @index = 1
+WHILE @index <= @num2022
+BEGIN
+	PRINT @index
+	SELECT @name = StateName FROM (
+		SELECT DISTINCT StateName, 
+		ROW_NUMBER() OVER( ORDER BY StateName) AS rownum
+		FROM sql_portfolio..StateLookup
+		WHERE StateName NOT IN (
+			SELECT State
+			FROM sql_portfolio..mass_shooting
+			WHERE YEAR([Incident Date]) = 2022
+			)) t
+	WHERE rownum = @index
+
+	INSERT INTO @table_addon (state, year, total_incidents, total_killed, total_injured, total_ck, total_ci, total_tk, total_ti)
+	VALUES (@name, 2022, 0, 0, 0, 0, 0, 0, 0)
+	SELECT @index = @index + 1
+END
+
+-- Insert new rows into gun_data_viz_table
+INSERT INTO sql_portfolio..gun_data_viz_table
+SELECT * FROM @table_addon
+-- Alter the view
+ALTER VIEW gun_data_viz
+AS 
+SELECT * FROM sql_portfolio..gun_data_viz_table
